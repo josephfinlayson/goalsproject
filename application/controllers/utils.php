0da53df03a776777185d7aa73e55f8006547778d
Joseph Finlayson <joe@mimsy.co>
2013-06-12 23:17:07 +0100

    starting email methods

---
+++ application/controllers/utils.php
@@ -0,0 +1,25 @@
+<?
+
+class utils extends CI_controller
+	{
+
+		 function __construct(){
+
+   parent::__construct();
+
+
+
+		}
+
+function sendConfirmationMail($array){
+
+	// send email to everyone else when goals are updated
+}
+
+function sendUpdateEmail($array){
+
+
+
+}
+
+}
