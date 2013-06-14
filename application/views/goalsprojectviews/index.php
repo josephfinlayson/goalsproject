44e538bf26512cc5bc5b15d40eecc089676543d6
joseph finlayson <joseph.finlayson@gmail.com>
2013-06-14 12:52:16 +0100

    editing index page

--- application/views/goalsprojectviews/index.php
+++ application/views/goalsprojectviews/index.php
@@ -9,7 +9,12 @@
     <div class="span9">
         <div class="hero-unit">
             <h1>Hello, lads!</h1>
-            <p>Welcome to the website to track goals. I promise that I'll make it better in the coming weeks</p>
+            <p>Welcome to the website to track goals. It's getting better and better</p>
+            <p> Did you know? </p>
+            <ul>
+            	<li> The goals tracker remembers your previous goals so you don't have to retype them</li>
+            	<li> The goals tracker emails us all automatically when you upload new goals</li>
+            </ul>
             <p><?php echo anchor('goals/questions',"Start here »", array("class" => "btn btn-large btn-primary"));?></p>
         </div>	
     </div>
