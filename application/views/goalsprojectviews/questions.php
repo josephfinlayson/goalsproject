12bc2ef0ae1cca0938683efcfd7605c4f0b03272
joseph finlayson <joseph.finlayson@gmail.com>
2013-07-10 06:24:06 +0100

    creating panel

--- application/views/goalsprojectviews/questions.php
+++ application/views/goalsprojectviews/questions.php
@@ -19,7 +19,7 @@ echo form_open('goals/questions_form', $attributes); ?>
         <br /><input id="person" type="text" name="person" class="input-xlarge uneditable-input" style = "display:none" value="<?php echo $query[0]['person']; ?>"  />
 </p>
 
-<p>
+<div>
         <label for="q1"><h3>Query 1:</h3></label>
         <?php echo form_error('q1'); ?>
         <input id="q1"  type="text" name="q1" class="input-xxlarge"  value="<?php echo $query[0]['q1']; ?>"  />
@@ -28,9 +28,9 @@ echo form_open('goals/questions_form', $attributes); ?>
         <?php echo form_error('a1'); ?>
         <br /><textarea rows="6" id="a1" class="span5" type="text" name="a1" placeholder="Type something…" value="<?php echo set_value('a1'); ?>"></textarea>
 
-</p>
-         <div class = "prevanswers" style="display:inline">Last week you said: <?=$query[0]['a1']?> </div>
-<p>
+</div>
+         <div class = "prevanswers"><p>Last week you said: </p><?=$query[0]['a1']?> </div>
+<div>
         <label for="q2"><h3>Query 2:</h3></label>
         <?php echo form_error('q2'); ?>
         <input id="q2" type="text" name="q2" class="input-xxlarge"  value="<?php echo $query[0]['q2']; ?>"  />
@@ -38,27 +38,27 @@ echo form_open('goals/questions_form', $attributes); ?>
      
         <?php echo form_error('a2'); ?>
         <br /><textarea rows="6" id="a2" class="span5" type="text" name="a2" placeholder="Type something…" value="<?php echo set_value('a2'); ?>"></textarea>
-        <div class = "prevanswers" style="display:inline">Last week you said: <?=$query[0]['a2']?> </div> 
-</p>
-<p>
+     </div>
+        <div class = "prevanswers">Last week you said: <?=$query[0]['a2']?> </div> 
+<div>
         <label for="q3"><h3>Query 3:</h3></label>
         <?php echo form_error('q3'); ?>
         <input id="q3" type="text" name="q3" class="input-xxlarge"  value="<?php echo $query[0]['q3']; ?>"  />
 
         <?php echo form_error('a3'); ?>
         <br /><textarea rows="6" id="a3" class="span5" type="text" name="a3" placeholder="Type something…" value="<?php echo set_value('a3'); ?>"></textarea>
+     </div>
         <div class = "prevanswers">Last week you said: <?=$query[0]['a3']?> </div>
-</p>
-
-<p>
+<div>
         <label for="sq"><h3>Summary Question</h3></label>
         <?php echo form_error('sq'); ?>
         <input id="sq" type="text" name="sq" class="input-xxlarge"  value="<?php echo $query[0]['sq']; ?>"  />
 
         <?php echo form_error('sa'); ?>
         <br /><textarea rows="6" id="sa" class="span5" type="text" name="sa" placeholder="Type something…" value="<?php echo set_value('sa'); ?>"></textarea>
-         <div class = "prevanswers" style="display:inline">Last week you said: <?=$query[0]['sa']?> </div>
-</p>
+     </div>
+         <div class = "prevanswers">Last week you said: <?=$query[0]['sa']?> </div>
+
 
 
 <p>
